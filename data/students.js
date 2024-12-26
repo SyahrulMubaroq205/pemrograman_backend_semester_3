adc6d62a7be8ba8f2d17326ad7bc29288b1c6fcc
SyahrulMubaroq205 <syahrulmubaroq0505@gmail.com>
2024-12-26 19:27:30 +0700

    praktikum11

---
+++ data/students.js
@@ -0,0 +1,21 @@
+// TODO 1: Buat data students
+const students = [
+    {
+      id: 1,
+      name: "Irham",
+      age: 20,
+    },
+    {
+      id: 2,
+      name: "Diaz Farindra",
+      age: 21,
+    },
+    {
+      id: 3,
+      name: "Dzaka Dika",
+      age: 22,
+    },
+  ];
+  
+  // TODO 2: export data students
+  export default students;
